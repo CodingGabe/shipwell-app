c34f3d29ebd64a79ca98bd1f84a394bc9ff30c11
Gabe Amaya <gabeamaya2@gmail.com>
2019-11-10 20:27:30 -0600

    add react form hooks for faster validation rules and create button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FormField from './FormField';
+import SubmitBtn from './SubmitBtn';
+import useForm from 'react-hook-form';
 
 const Form = () => {
+    const { register, handleSubmit, watch, errors } = useForm()
+    const onSubmit = data => { console.log(data) }
+
+    console.log(watch('howzit'))
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <FormField
                 label={'Stop Name'}
             />
-            {/* <SubmitBtn /> */}
+            <FormField
+                label={'Stop Address'}
+            />
+            <SubmitBtn buttonText="Add Stop"/>
         </form>
     )
 }
---
+++ src/components/SubmitBtn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubmitBtn = (props) => {
+    return <button type="submit" className="shipwell-btn">{props.buttonText}</button>
+}
+
+export default SubmitBtn;
